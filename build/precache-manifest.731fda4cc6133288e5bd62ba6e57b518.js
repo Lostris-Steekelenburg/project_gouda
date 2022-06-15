974abcd4a24efcc03259044696b15b5e4ed11bc5
Lostris-Steekelenburg <s1121524@student.hsleiden.nl>
2022-06-15 11:11:25 +0200

    added build files

---
+++ build/precache-manifest.731fda4cc6133288e5bd62ba6e57b518.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82c98a0622db36ef185f",
+    "url": "/project_gouda/static/css/main.d7cd8eeb.chunk.css"
+  },
+  {
+    "revision": "82c98a0622db36ef185f",
+    "url": "/project_gouda/static/js/main.82c98a06.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/project_gouda/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/project_gouda/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "60498cf3213faefa0687",
+    "url": "/project_gouda/static/js/runtime~main.60498cf3.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/project_gouda/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "24d7b4107007f73dfe3f539dbf92d91c",
+    "url": "/project_gouda/index.html"
+  }
+];
