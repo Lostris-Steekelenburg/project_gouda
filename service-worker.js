36f57bd3e30c27e8a15c34d32211d9a59043bb3d
Lostris-Steekelenburg <s1121524@student.hsleiden.nl>
2022-06-15 11:09:37 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5217d1ffcad3b1f0d5e1501e0a9cac08.js"
+  "/project_gouda/precache-manifest.731fda4cc6133288e5bd62ba6e57b518.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/project_gouda/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
