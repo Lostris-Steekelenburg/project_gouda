12cccb10e0ee688509af91abb8bc6fe4802c1e38
Tim Roks <h.p.t.roks@gmail.com>
2022-06-19 23:13:48 +0200

    changed default marker

--- src/Map.js
+++ src/Map.js
@@ -8,8 +8,8 @@ import iconGroen from './img/markerGroen.png'
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-   iconRetinaUrl: require('./img/marker-blauw-2x.png'),
-   iconUrl: require('./img/marker-blauw.png'),
+   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+   iconUrl: require('leaflet/dist/images/marker-icon.png'),
    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
