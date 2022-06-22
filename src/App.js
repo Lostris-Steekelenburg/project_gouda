f1201229bd1115ef2f734d84e640d3a9b7e4c57b
Lostris-Steekelenburg <s1121524@student.hsleiden.nl>
2022-06-23 00:07:09 +0200

    test

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
       <div className="contentContainer">
       <header className="contentContainer__header"><h1 className="contentContainer__header__h1">Gouda in Beeld & Gedicht</h1></header>
       <section className = "button__section">
-        <a href="./alleRoutes">
+        <a href="./lostris-steekelenburg.github.io/project_gouda/alleRoutes">
           <button className='button__alle'>Alle</button>
         </a>
         <a href="./groeneRoute">
@@ -82,12 +82,21 @@ class App extends Component {
       </div>
           <hr />
           <Switch>
+              <Route path='/lostris-steekelenburg.github.io/project_gouda/alleRoutes' component={alleRoutes} />
+              <Route path='/groeneRoute' component={GroeneRoute} />
+              <Route path='/geleRoute' component={GeleRoute} />
+              <Route path='/rodeRoute' component={RodeRoute} />
+              <Route path='/blauweRoute' component={BlauweRoute} />
+          </Switch> 
+
+          {/* dit is voor npm start op lokale server
+          <Switch>
               <Route path='/alleRoutes' component={alleRoutes} />
               <Route path='/groeneRoute' component={GroeneRoute} />
               <Route path='/geleRoute' component={GeleRoute} />
               <Route path='/rodeRoute' component={RodeRoute} />
               <Route path='/blauweRoute' component={BlauweRoute} />
-          </Switch>
+          </Switch> */}
       </Router>
     );
   }
