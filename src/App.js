22b921b4cdb1ff947d00355e4838571f0092732f
Lostris-Steekelenburg <s1121524@student.hsleiden.nl>
2022-06-23 01:28:21 +0200

    mmhhh

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ class App extends Component {
     <Router>
       <div className="contentContainer">
       <header className="contentContainer__header"><h1 className="contentContainer__header__h1">Gouda in Beeld & Gedicht</h1></header>
-      {/* Voor de github */}
-      <section className = "button__section"> =
+      <section className = "button__section"> 
         <a href="./alleRoutes">
           <button className='button__alle'>Alle</button>
         </a>
@@ -32,24 +31,6 @@ class App extends Component {
         </a>
       </section>
 
-      {/* Voor het lokaal testen */}
-      {/* <section className = "button__section">
-        <a href="/project_gouda/alleRoutes">
-          <button className='button__alle'>Alle</button>
-        </a>
-        <a href="/project_gouda/groeneRoute">
-          <button className='button button__groen'>Groen</button>
-        </a>
-        <a href="/project_gouda/geleRoute">
-          <button className='button__geel'>Geel</button>
-        </a>
-        <a href="/project_gouda/rodeRoute">
-          <button className='button__rood'>Rood</button>
-        </a>
-        <a href="/project_gouda/blauweRoute">
-          <button className='button__blauw'>Blauw</button>
-        </a>
-      </section> */}
       </div>
           <hr />
           <Switch>
